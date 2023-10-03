dc18d1fd5d906ebacda80690e4712e09e84cca6e
drp014 <paduan582@gmail.com>
2023-10-03 15:47:37 -0300

    projeto_integrador

---
+++ models/login_model.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once("util/param.php");
+
+class Login_Model extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function example()
+    {
+    }
+}
---
+++ views/login/login.vue.js
@@ -0,0 +1,21 @@
+const AppTemplate = `
+<div class="control-section" style="margin-top: 5%;">
+    <div class="login-page">
+    </div>
+</div>
+`;
+
+
+Vue.component("AppVue", {
+  template: AppTemplate,
+  data() {
+    return {
+        input: {
+        }
+    }
+  },
+    methods: {
+    },
+    mounted() {
+    },
+});
