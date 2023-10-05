33e3e21cb15a96451a0a3a214363df3dd153a985
Giih Gabi <giovannacostabatista@gmail.com>
2023-10-05 18:42:12 -0300

    Cadastro Terminado

--- views/loginPage/app.vue.js
+++ views/loginPage/app.vue.js
@@ -2,7 +2,6 @@ const AppTemplate = `
 <div id="forms-cad" style='height: 100vh;'>
     <div id="form-right">
         <div id="formulario-cadastro" class="row">
-
             <!-- ******************EMAIL*************** -->
             <div class="row">
                 <div class="col-md-8 margin-input">
@@ -15,15 +14,15 @@ const AppTemplate = `
                 </div>
             </div>                
             <!-- ******************SENHA*************** -->
-            <div class="row margin-input">
+            <div class="row margin-input" style="margin-top: 6px;">
                 <div class="col-md-8 margin-input">
                     <ejs-textbox floatLabelType="Auto" cssClass="e-outline" placeholder="Senha"></ejs-textbox>
                 </div>
             </div>
             <!-- BOTÃO -->
-            <div class="row">
-                <div class="col-md-12 margin-input">
-                    <ejs-progressbutton id="zoomin" content="Zoom In" :enableProgress="true" :spinSettings="spinCenter" :animationSettings="zoomIn"
+            <div class="row text-center" style="margin-top: 2em">
+                <div class="col-md-8 margin-input">
+                    <ejs-progressbutton id="zoomin" content="ENTRAR" :enableProgress="true" :spinSettings="spinCenter" :animationSettings="zoomIn"
                     cssClass="e-round-corner"></ejs-progressbutton>
                 </div>
             </div>
