a74448e7190a93193ab30109521b369381142c29
Rhyzert <leoflorentino97@gmail.com>
2023-10-06 17:13:24 -0300

    LeozinhoLegal

--- models/cadastroLogin_model.php
+++ models/cadastroLogin_model.php
@@ -12,7 +12,56 @@ class CadastroLogin_Model extends Model
     public function Cadastrar_usuario()
     {
         $post = json_decode(file_get_contents('php://input'));
-        var_dump($post);exit;
+        //var_dump($post);exit;
+        $nome = $post->NOME;
+        $CPF = $post->CPF;
+        $email = $post->EMAIL;
+        $senha = $post->SENHA;
+        $confirm_senha = $post->CONFIRM_SENHA;
+
+        /* VALIDAR E-MAIL */
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            true;
+        } else{
+            exit(json_encode(array("code" => "0", "msg" => "Opss!! E-mail está inválido!!")));
+        }
+
+        /* VALIDAR CPF */
+        $valid_cpf = validaCPF($CPF);
+        if($valid_cpf == false){
+            exit(json_encode(array("code" => "0", "msg" => "Opss!! CPF inválido!!")));
+        }
+
+        /* VALIDAR SENHA */
+        if($senha != $confirm_senha){
+            exit(json_encode(array("code" => "0", "msg" => "Senha diferentes!!.Por favor, digite novamente.")));
+        }
+
+        /* DECODIFICAÇAO DA SENHA */
+        $senha_hash = hash('sha256', $senha_final);
+
+        if($nome == null){
+            exit(json_encode(array("code" => "0", "msg" => "Por favor, insira o Nome.")));
+        }
+        else if($CPF == null){
+            exit(json_encode(array("code" => "0", "msg" => "Por favor, insira o CPF.")));
+        }
+        else if($email == null){
+            exit(json_encode(array("code" => "0", "msg" => "Por favor, insira o E-mail.")));
+        }
+        else {
+            $nome = strtoupper($nome);
+            $email = strtolower($email);
+
+            $result = $this->db->insert('USUARIO', array('NOME' =>$nome, 'CPF' => $CPF, 'EMAIL' => $email, 'SENHA' => $senha_hash));
+
+            if($result){
+                exit(json_encode(array("code" => "1", "msg" => "Cadastro realizado com sucesso.")));
+            } else{
+                exit(json_encode(array("code" => "0", "msg" => "Erro ao inserir.")));
+            }
+        }
+        echo (json_encode($msg));
 
     }
 }
